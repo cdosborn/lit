f076eb334986a5e7a4fae43fdbd9d5e57c8b36d0
Connor Osborn <cdosborn@uw.edu>
2014-08-16 17:31:33 -0600

    watch cmd polls less frequently

--- src/Poll.hs
+++ src/Poll.hs
@@ -14,24 +14,24 @@ watch fun fs = do
     mapM_ fun fs
      -- total microseconds for each file to cause a 1 sec delay per loop
     let delay = 1000000 `div` (length fs)
-    forever $ mapM_ (onDiff fun delay) fs
+    forever $ (C.threadDelay 1000000 >> mapM_ (onDiff fun delay) fs)
 
 onDiff :: (String -> IO ()) -> Int -> String -> IO ()
 onDiff fun delay file = do
     modified <- errorHandler (getModificationTime file) 
     curTime <- getCurrentTime 
     let diff = (diffUTCTime curTime modified)
-    if diff < 1 then fun file >> C.threadDelay delay else return ()
+    if diff < 2 then fun file >> C.threadDelay delay else return ()
  
 
 -- a really conservative check to prevent file
 -- "inavailability" due to reading modification bits
-errorHandler = errorHandlerNTimes 100
+errorHandler = errorHandlerNTimes 10
 
 errorHandlerNTimes 0 mnd = catchIOError mnd (\e -> ioError e)
-errorHandlerNTimes times mnd = catchIOError mnd handle 
+errorHandlerNTimes times mnd = {-(putStrLn $ show times) >>-} catchIOError mnd handle 
     where   
         handle e =
             if isDoesNotExistError e 
-            then C.threadDelay 5000 >> errorHandlerNTimes (times - 1) mnd
+            then C.threadDelay 50000 >> errorHandlerNTimes (times - 1) mnd
             else ioError e
