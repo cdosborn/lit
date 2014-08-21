4facb7be644dc343d9931d0c12d41c5b90c17fd4
Connor Osborn <cdosborn@uw.edu>
2014-08-20 23:03:22 -0600

    repaired bug, introduced by refactoring

--- src/Parse.hs
+++ src/Parse.hs
@@ -13,6 +13,57 @@ encode txt =
     Left err -> []
     Right result -> result
 
+entire :: Parser Program
+entire = manyTill chunk eof
+
+chunk :: Parser Chunk
+chunk = (try def) <|> prose
+
+prose :: Parser Chunk
+prose = grabLine >>= (\line -> return $ Prose line)
+
+def :: Parser Chunk
+def = do
+    (indent, header, lineNum) <- title
+    parts <- manyTill (part indent) $ endDef indent
+    return $ Def lineNum header parts
+
+part :: String -> Parser Part
+part indent = 
+    try (string indent >> varLine) <|> 
+    (many ws >> grabLine >>= \extra -> return $ Code extra)
+
+varLine :: Parser Part
+varLine = do
+    name <- packM =<< between (string "<<") (string ">>") (many notDelim)
+    newline
+    return $ Ref name
+
+endDef :: String -> Parser ()
+endDef indent = try $ do { skipMany newline; notFollowedBy (string indent) <|> (lookAhead title >> parserReturn ()) }
+
+grabLine :: Parser T.Text
+grabLine = do 
+    line <- many (noneOf "\n\r")
+    last <- newline
+    return $ T.pack $ line ++ [last]
+
+packM str = return $ T.pack str
+
+-- Pre: Assumes that parser is looking at a fresh line with a macro defn
+-- Post: Returns (indent, macro-name, line-no)
+title :: Parser (String, T.Text, Int)
+title = do
+    pos <- getPosition
+    indent <- many ws
+    name <- fmap T.pack $ between (string "<<") (string ">>=") (many notDelim)
+    newline
+    return $ (indent, T.strip name, sourceLine pos)
+
+notDelim = noneOf ">="
+ws :: Parser Char
+ws = char ' ' <|> char '\t'
+
 textP :: Parsec T.Text () T.Text ->  T.Text -> T.Text
 textP p txt =
     case (parse p "" txt) of 
@@ -24,67 +75,3 @@ chunkP p txt =
     case (parse p "" txt) of 
     Left err -> Nothing
     Right result -> Just result
-
-entire :: Parser Program
-entire = manyTill chunk eof
-
-chunk :: Parser Chunk
-chunk = (try def) <|> prose
-
-prose :: Parser Chunk
-prose = do 
-    txt <- packM =<< many (noneOf "\n\r")
-    nl <- eol >>= (\c -> return $ T.singleton c)
-    return $ Prose (txt `T.append` nl)
-
-def :: Parser Chunk
-def = do
-    (indent, header, lineNum) <- title
-    parts <- manyTill (part indent) $ endDef indent
-    return $ Def lineNum header parts
-
-part :: String -> Parser Part
-part indent =
-    try (string indent >> varLine) <|> 
-    try (string indent >> defLine) <|> 
-    (grabLine >>= (\extra -> return (Code $ extra)))
-
-varLine :: Parser Part
-varLine = do
-    name <- packM =<< between (string "<<") (string ">>") (many notDelim)
-    eol
-    return $ Ref name
-
-defLine :: Parser Part
-defLine = do
-    line <- grabLine 
-    return $ Code line
-
-endDef :: String -> Parser ()
-endDef indent = try $ do { skipMany newline; notFollowedBy (string indent) <|> (lookAhead title >> parserReturn ()) }
-
-beginDef = try $ do {lookAhead title >> parserReturn ()}
-
-grabLine :: Parser T.Text
-grabLine = do 
-    line <- packM =<< many (noneOf "\n\r")
-    last <- eol >>= (\c -> return $ T.singleton c)
-    return $ line `T.append` last
-
-packM str = return $ T.pack str
-
--- Pre: Assumes that parser is looking at a fresh line with a macro defn
--- Post: Returns (indent, macro-name, line-no)
-title :: Parser (String, T.Text, Int)
-title = do
-    pos <- getPosition
-    indent <- many ws
-    name <- packM =<< between (string "<<") (string ">>=") (many notDelim)
-    eol
-    return $ (indent, T.strip name, sourceLine pos)
-
-notDelim = noneOf ">="
-ws :: Parser Char
-ws = char ' ' <|> char '\t'
-eol :: Parser Char
-eol = char '\n' <|> char '\r'
